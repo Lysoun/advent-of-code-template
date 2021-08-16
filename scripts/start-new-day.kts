b52d1879e1e878b5bcd0540b93a4380d6bd9447e
Élise Rubio <elise.rubio@4sh.fr>
2021-08-16 12:14:57 +0200

    :recycle: Extract file related methods to util-file

--- scripts/start-new-day.kts
+++ scripts/start-new-day.kts
@@ -1,43 +1,22 @@
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
 import java.time.LocalDate
 
+@file:Include("util-file.kts")
 @file:Include("util-git.kts")
 
-        /**
-         * Returns true if folder was created
-         */
-fun createFolder(day: Int): String {
-    println("Try creating folder for day $day")
+fun createDayFolder(day: Int): String {
     val folderName = "day$day"
-
-    if (File(folderName).exists()) {
-        println("Folder was not created because it already existed")
-    } else {
-        println("Create folder $folderName")
-        Files.createDirectory(Paths.get(folderName))
-    }
+    createFolder(folderName)
     return folderName
 }
 
-fun createPart1File(folderName: String): Boolean {
-    println("Try creating part 1 file in folder $folderName")
-    val templatePath = Paths.get("scripts/templates/solution_template.ml")
-    val part1FileName = "${folderName}/part1.ml"
-
-    if (File(part1FileName).exists()) {
-        println("Part 1 file was not created because it already existed")
-        return false
-    }
-
-    Files.copy(templatePath, Paths.get(part1FileName))
-    return true
-}
+fun createPart1File(folderName: String): Boolean = createFileUsingTemplate(
+    fileName = "${folderName}/part1.ml",
+    templateFilePath = "scripts/templates/solution_template.ml"
+)
 
 val currentDateDay = LocalDate.now().dayOfMonth
 
-val folderName = createFolder(currentDateDay)
+val folderName = createDayFolder(currentDateDay)
 val part1FileCreated = createPart1File(folderName)
 if (part1FileCreated) {
     println("Commit files for day $currentDateDay")
---
+++ scripts/util-file.kts
@@ -0,0 +1,36 @@
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+
+/**
+ * Create folder if possible, return boolean depending on whether it was created
+ */
+fun createFolder(folderName: String): Boolean {
+    println("Try creating folder $folderName")
+
+    if (File(folderName).exists()) {
+        println("Folder $folderName was not created because it already existed")
+        return false
+    }
+
+    println("Create folder $folderName")
+    Files.createDirectory(Paths.get(folderName))
+
+    return true
+}
+
+/**
+ * Create file using template file if possible, return boolean depending on whether it was created
+ */
+fun createFileUsingTemplate(fileName: String, templateFilePath: String): Boolean {
+    println("Try creating file $fileName")
+
+    if (File(fileName).exists()) {
+        println("File $fileName was not created because it already existed")
+        return false
+    }
+
+    println("Create file $fileName")
+    Files.copy(Paths.get(templateFilePath), Paths.get(fileName))
+    return true
+}
