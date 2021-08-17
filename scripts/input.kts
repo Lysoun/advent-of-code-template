301000b3aa4432a7e9dac77d2db32619d9ba09ce
Élise Rubio <elise.rubio@4sh.fr>
2021-08-17 23:29:10 +0200

    :recycle: Extract input loading to file

---
+++ scripts/input.kts
@@ -0,0 +1,31 @@
+@file:Include("util-http.kts")
+@file:Include("util-file.kts")
+@file:Include("util-git.kts")
+@file:Include("util-config.kts")
+
+
+fun retrieveInput(day: Int, sessionId: String): String? {
+    val url = "https://adventofcode.com/2020/day/$day/input"
+    return get(url, sessionId)
+}
+
+fun writeInput(folderName: String, input: String) {
+    writeToFile(
+        "$folderName/input.txt",
+        input
+    )
+}
+
+fun loadInput(day: Int, folderName: String) {
+    val input = retrieveInput(currentDateDay, loadSessionId())
+
+    if (input == null) {
+        println("Input was empty; no file was written")
+    } else {
+        println("Writing input for day $currentDateDay into file")
+        writeInput(folderName, input)
+        println("Commit input for day $currentDateDay")
+        commitInput(currentDateDay)
+    }
+}
+
--- scripts/start-new-day.kts
+++ scripts/start-new-day.kts
@@ -1,7 +1,6 @@
 @file:Include("util-file.kts")
 @file:Include("util-git.kts")
-@file:Include("util-http.kts")
-@file:Include("util-config.kts")
+@file:Include("input.kts")
 
 import java.time.LocalDate
 
@@ -17,18 +16,6 @@ fun createPart1File(folderName: String): Boolean = createFileUsingTemplate(
     templateFilePath = "scripts/templates/solution_template.ml"
 )
 
-fun retrieveInput(day: Int, sessionId: String): String? {
-    val url = "https://adventofcode.com/2020/day/$day/input"
-    return get(url, sessionId)
-}
-
-fun writeInput(folderName: String, input: String) {
-    writeToFile(
-        "$folderName/input.txt",
-        input
-    )
-}
-
 val currentDateDay = LocalDate.now().dayOfMonth
 
 val folderName = createDayFolder(currentDateDay)
@@ -37,13 +24,5 @@ if (part1FileCreated) {
     println("Commit files for day $currentDateDay")
     commitDaySolutionsInit(currentDateDay)
 }
-val input = retrieveInput(currentDateDay, loadSessionId())
 
-if (input == null) {
-    println("Input was empty; no file was written")
-} else {
-    println("Writing input for day $currentDateDay into file")
-    writeInput(folderName, input)
-    println("Commit input for day $currentDateDay")
-    commitInput(currentDateDay)
-}
+loadInput(currentDateDay, folderName)
